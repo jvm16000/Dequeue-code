8ac524648bf3efcc21c4ada3e1489554f00e1d19
Jay Mehta <60725742+jvm16000@users.noreply.github.com>
2020-08-12 23:15:29 +0530

    Add files via upload

---
+++ dequeue.py
@@ -0,0 +1,13 @@
+arr = []
+def is_empty(c):
+	if c.len() == 0
+	return True
+
+def appendright(c):
+	arr.append(c)
+
+def removeleftelement():
+	if is_empty:
+		return ("list is empty")
+	else:
+		arr.pop()
